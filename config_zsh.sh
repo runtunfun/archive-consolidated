033f6b98dcbf2815a6acacc6fba6ca7d2067e4c2
SteKu73 <61452729+SteKu73@users.noreply.github.com>
2022-05-16 09:55:01 +0200

    Update config_zsh.sh

--- config_zsh.sh
+++ config_zsh.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install curl git yadm zsh
-# git config --global user.name "Stefan Kuehn"; git config --global user.email "stefan.kuehn@etomer.com"
+# git config --global user.name "Ste Ku"; git config --global user.email "stefan.kuehn.1973@gmail.com"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
