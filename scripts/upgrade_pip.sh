e18f33cbff1a72b240674814d9f52ae370895030
Ste Ku <stefan.kuehn.1973@gmail.com>
2022-08-16 22:46:25 +0200

    hardened upgrade_pip.sh

--- scripts/upgrade_pip.sh
+++ scripts/upgrade_pip.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for i in `pip list --outdated | egrep -v '^Package|^----' | cut -d " " -f 1`
+PYTHON=$(which python)
+echo ${PYTHON}
+${PYTHON} -m pip install --upgrade pip
+for PIP in `pip list --outdated | egrep -v '^Package|^----' | cut -d " " -f 1`
 do
-pip install --upgrade ${i}
+pip install --upgrade ${PIP}
 done
