a14ea774f8fe5dadc29458672f448184e5330b99
stefan <runtunfun@gmail.com>
2023-08-16 19:12:21 +0200

    chore(main): cleanup personal data

--- scripts/list_dpkg.sh
+++ scripts/list_dpkg.sh
@@ -3,7 +3,7 @@
 
 for SERVER in `grep "Host " ~/.ssh/config.d/* | grep -v ds | cut -d " " -f 2 | sort -u`                                                                             ─╯
 do
-ssh wartung@${SERVER} hostname -f > 20220817a_${SERVER}_dpkg-l.csv
-ssh wartung@${SERVER} uname -s >> 20220817a_${SERVER}_dpkg-l.csv
-ssh wartung@${SERVER} dpkg --list | awk '/^ii/ {print $2}' | grep -v ^lib | sort -u >> 20220817a_${SERVER}_dpkg-l.csv
+ssh USER@${SERVER} hostname -f > 20220817a_${SERVER}_dpkg-l.csv
+ssh USER@${SERVER} uname -s >> 20220817a_${SERVER}_dpkg-l.csv
+ssh USER@${SERVER} dpkg --list | awk '/^ii/ {print $2}' | grep -v ^lib | sort -u >> 20220817a_${SERVER}_dpkg-l.csv
 done
