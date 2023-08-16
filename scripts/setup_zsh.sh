2450004445babffb6fac0d43a7c022b99f3368f3
Ste Ku <stefan.kuehn.1973@gmail.com>
2023-08-16 22:46:16 +0200

    chore(main): added sw to setup

--- scripts/setup_zsh.sh
+++ scripts/setup_zsh.sh
@@ -2,12 +2,11 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install curl git yadm zsh -y
+sudo apt-get install curl git unzip wget yadm zip zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # code .zshrc
-# change Theme "powerlevel10k/powerlevel10k" und plugin "git zsh-autosuggestions"
-# yadm clone git@support2.etomer.com:sk0001/dotfiles.git
+# change Theme "powerlevel10k/powerlevel10k" and plugin "git zsh-autosuggestions"
 # cp ~/.zshrc ~/.zshrc.after-oh-my-zsh
 # cp ~/.zshrc.pre-oh-my-zsh ~/.zshrc
